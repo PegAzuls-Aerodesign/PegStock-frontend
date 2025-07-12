89c2e696c4e57ff91cd9c6e0e4ccdb85661f8e9a
vynijales <vynijales@gmail.com>
2025-07-12 11:11:55 -0300

    feat(ui): add header content to MaterialTable and improve default values for empty fields

--- src/core/material/ui/material-table.tsx
+++ src/core/material/ui/material-table.tsx
@@ -1,6 +1,8 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
 import { DataTable } from "@/components/ui/data-table";
+import Link from "next/link";
 import { useMateriais } from "../material.service";
 
 export const MaterialTable = () => {
@@ -14,20 +16,27 @@ export const MaterialTable = () => {
         "Categoria",
         "Armário",
         "Data de Validade",
+        // "Status",
       ]}
       data={
         materiais.data?.map((material) => ({
-          Produto: material.name || "",
-          Quantidade: material.quantitiy || "",
-          Categoria: material.category || "",
-          Armário: material.box || "",
+          Produto: material.name || "-",
+          Quantidade: material.quantitiy || "-",
+          Categoria: material.category || "-",
+          Armário: material.box || "-",
           "Data de Validade": material.expirationDate
             ? material.expirationDate.toLocaleDateString("pt-BR")
-            : "",
+            : "-",
+          // Status:
           id: material.cod,
         })) || []
       }
       isLoading={materiais.isLoading}
+      headerContent={
+        <Button variant="primary" className="w-32" asChild>
+          <Link href="/estoque/criar">Novo material</Link>
+        </Button>
+      }
     />
   );
 };
