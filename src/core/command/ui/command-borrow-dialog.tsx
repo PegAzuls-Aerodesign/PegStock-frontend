5dff7e7dbe68abb1d528d298e9ac34ff901ec1eb
vynijales <vynijales@gmail.com>
2025-07-23 19:34:04 -0300

    feat(ui): add state management for open/close dialog in borrow and material dialogs
    when the form submission is accepted, the modal will close

--- src/core/command/ui/command-borrow-dialog.tsx
+++ src/core/command/ui/command-borrow-dialog.tsx
@@ -1,5 +1,6 @@
 import { Button } from "@/components/ui/button";
 import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
+import { useState } from "react";
 import { CommandBorrowForm } from "./command-borrow-form";
 
 interface DialogProps {
@@ -9,15 +10,20 @@ interface DialogProps {
 }
 
 export const CommandBorrowDialog: React.FC<DialogProps> = (props) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const handleClose = () => {
+    setIsOpen(false);
+  };
+
   return (
-    <Dialog>
+    <Dialog open={isOpen} onOpenChange={setIsOpen}>
       <DialogContent
         className="z-[60] max-h-[90vh] w-full max-w-4xl overflow-y-auto rounded-none border-0 bg-transparent p-0"
         overlayClassName="z-[55]"
       >
         <CommandBorrowForm
           materialCod={props.materialCod}
-          onSuccess={props.onSuccess}
+          onSuccess={handleClose}
           hideFooter
         />
       </DialogContent>
--- src/core/command/ui/command-material-dialog.tsx
+++ src/core/command/ui/command-material-dialog.tsx
@@ -1,5 +1,6 @@
 import { Button } from "@/components/ui/button";
 import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
+import { useState } from "react";
 import { CommandMaterialForm } from "./command-material-form";
 
 interface DialogProps {
@@ -13,9 +14,10 @@ export const CommandMaterialDialog: React.FC<DialogProps> = ({
   type,
   ...props
 }) => {
+  const [isOpen, setIsOpen] = useState(false);
   const buttonName = type === "add" ? "Adicionar Material" : "Remover Material";
   return (
-    <Dialog>
+    <Dialog open={isOpen} onOpenChange={setIsOpen}>
       <DialogContent
         className="z-[60] max-h-[90vh] w-full max-w-4xl overflow-y-auto rounded-none border-0 bg-transparent p-0"
         overlayClassName="z-[55]"
